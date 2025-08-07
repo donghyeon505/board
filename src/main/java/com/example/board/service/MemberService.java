246155d4db95d7b428f7edfe8332adfb103df2c8
donghyeon505 <ehdgus458@gmail.com>
2025-08-07 11:33:07 +0900

    feat: (실습) 유저 조회 기능 추가

--- src/main/java/com/example/board/service/MemberService.java
+++ src/main/java/com/example/board/service/MemberService.java
@@ -1,10 +1,13 @@
 package com.example.board.service;
 
+import com.example.board.dto.MemberResponseDto;
 import com.example.board.dto.SignUpResponseDto;
 import com.example.board.entity.Member;
 import com.example.board.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -25,4 +28,10 @@ public class MemberService {
         );
     }
 
+    public MemberResponseDto findById(Long id) {
+        Member memberResponse = memberRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT));
+
+        return new MemberResponseDto(memberResponse.getUsername(), memberResponse.getAge());
+    }
 }
