0c942e1cb04428171658b5fb754a850b4c9e46a0
donghyeon505 <ehdgus458@gmail.com>
2025-08-07 11:48:49 +0900

    feat: (실습) 비밀번호 변경 기능 추가

--- src/main/java/com/example/board/service/MemberService.java
+++ src/main/java/com/example/board/service/MemberService.java
@@ -7,6 +7,7 @@ import com.example.board.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -15,6 +16,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     public SignUpResponseDto signUp(String username, String password, Integer age) {
 
         Member member = new Member(username, password, age);
@@ -28,10 +30,22 @@ public class MemberService {
         );
     }
 
+    @Transactional
     public MemberResponseDto findById(Long id) {
-        Member memberResponse = memberRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT));
+        Member memberResponse = memberRepository.findByIdOrElseThrow(id);
 
         return new MemberResponseDto(memberResponse.getUsername(), memberResponse.getAge());
+    }
+
+    @Transactional
+    public void updatePassword(Long id, String oldPassword, String newPassword) {
+
+        Member findMember = memberRepository.findByIdOrElseThrow(id);
+
+        if (!findMember.getPassword().equals(oldPassword)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다");
+        }
+
+        findMember.updatePassword(newPassword);
     }
 }
